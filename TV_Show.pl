ea7df6cb54036c669c40aac6947ec350268dfde1
emiliegp <egrandpi@bowdoin.edu>
2022-11-10 11:15:23 -0500

    Notes

---
+++ TV_Show.pl
@@ -0,0 +1,32 @@
+/*
+Notes:
+    - list of characters
+    - types of stories (general plot)
+    - what the characters do
+    - the charcters relationship with each other
+    - the environment characters are in 
+    - major conflict
+
+Making up our Mystery Story:
+    Characters:
+        1. Villian 
+        2. Hero
+        3. Suspect  
+        Character Relations;
+            - filler characters that all fall into one of the above catagories 
+            - all villians are related to the victim in some way
+            - no hero is related to the villian 
+
+    Setting:
+        - incorperate overlapping layers so that certain queries can result in
+            both the hero/victim/suspect being associated with a place
+                ex. the vcitim and hero live in the same house
+    
+    Genral Plot:
+        - verbs that apply to areas of the setting 
+            ex. attending certain events/being in the same place at the same time
+
+    Major Conflict:
+        - the suspects are all capable of doing something to a victim but not
+            all suspects commited crimes   
+*/
